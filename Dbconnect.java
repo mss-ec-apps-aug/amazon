d70c9166afb6d9c7a920846fb118b32aa0fe3eaf
chanduramu0915 <chanduramu0915@gmail.com>
2021-08-13 11:55:11 +0530

    commited in to development branch

--- Dbconnect.java
+++ Dbconnect.java
@@ -1,4 +1,5 @@
-hi 
+im in development branch 
+hi all
 gud morning guys 
 i am chandradhar from Anantapur
 i want to go through all the DevOps tools within a month and  i have to prepare for the interviews.
